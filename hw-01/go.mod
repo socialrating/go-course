0263e355296c2c6eac9254d9ed90d146f496240d
socialrating <mdk_bol@bk.ru>
2024-11-17 15:06:31 +0500

    hw01

---
+++ hw-01/go.mod
@@ -0,0 +1,3 @@
+module github.com/socialrating/go-course/hw-01
+
+go 1.22.4
---
+++ hw-01/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	first := 10
+	second := 20
+	sum := first + second
+
+	suma := 10
+	sumb := sum - suma
+
+	fmt.Println("Результат сложения:", sum)
+	fmt.Println("Результат вычитания:", sumb)
+}
